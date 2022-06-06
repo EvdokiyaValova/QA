bd61fc625308cd6fec17b40228a972ec8f6a2ee4
EvdokiyaValova <evdokiyavalova@gmail.com>
2022-06-06 23:09:16 +0300

    TestCalculator

---
+++ Selenium/Calculator/Calculator/DataDrivenTestingCalculator/TestsCalculator.cs
@@ -0,0 +1,95 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DataDrivenTestingCalculator
+{
+    public class TestsCalculator
+    {
+        private ChromeDriver driver;
+
+
+
+       
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://number-calculator.nakov.repl.co/";            
+        }
+
+        [OneTimeTearDown]
+        public void TearDoun() 
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void Test_Calculator()
+        {
+            //Arrange
+            var field1 = driver.FindElement(By.Id("number1"));
+            var field2 = driver.FindElement(By.Id("number2"));
+            var operation = driver.FindElement(By.Id("operation"));
+            var calculateButton = driver.FindElement(By.Id("calcButton"));
+            var clearField = driver.FindElement(By.Id("resetButton"));
+            var resultField = driver.FindElement(By.Id("result"));
+
+            //Act
+            field1.SendKeys("7");
+            operation.SendKeys("+");
+            field2.SendKeys("2");
+            calculateButton.Click();
+            var expected = "Result: 9";
+
+            //Assert
+            Assert.That(expected, Is.EqualTo(resultField.Text));
+
+            clearField.Click();
+        }
+
+        [TestCase("3", "+", "6", "Result: 9")]
+        [TestCase("-3", "+", "6", "Result: 3")]
+        [TestCase("3", "+", "-6", "Result: -3")]
+        [TestCase("0", "+", "6", "Result: 6")]
+        [TestCase("3", "+", "0", "Result: 3")]
+        [TestCase("3", "-", "6", "Result: -3")]
+        [TestCase("7", "-", "6", "Result: 1")]
+        [TestCase("3", "-", "-6", "Result: 9")]
+        [TestCase("-3", "-", "-6", "Result: 3")]
+        [TestCase("0", "-", "6", "Result: -6")]
+        [TestCase("-3", "-", "0", "Result: -3")]
+        [TestCase("-3", "*", "0", "Result: 0")]
+        [TestCase("0", "*", "2", "Result: 0")]
+        [TestCase("-3", "*", "-3", "Result: 9")]
+        [TestCase("2", "*", "4", "Result: 8")]
+        [TestCase("2", "*", "-3", "Result: -6")]
+        [TestCase("-2", "*", "3", "Result: -6")]
+        [TestCase("4", "/", "2", "Result: 2")]
+        [TestCase("-4", "/", "2", "Result: -2")]
+        [TestCase("4", "/", "-2", "Result: -2")]
+        [TestCase("0", "/", "4", "Result: 0")]
+        [TestCase("4", "/", "0", "Result: Infinity")]
+        [TestCase("3", "/", "1.5", "Result: 2")]
+        public void Test_CalculatorWebApp(string num1, string op, string num2, string expectedResult)
+        {
+            //Arrange
+            var field1 = driver.FindElement(By.Id("number1"));
+            var field2 = driver.FindElement(By.Id("number2"));
+            var operation = driver.FindElement(By.Id("operation"));
+            var calculateButton = driver.FindElement(By.Id("calcButton"));
+            var clearField = driver.FindElement(By.Id("resetButton"));
+            var resultField = driver.FindElement(By.Id("result"));
+
+            //Act
+            clearField.Click();
+            field1.SendKeys(num1);
+            operation.SendKeys(op);
+            field2.SendKeys(num2);
+            calculateButton.Click();
+            var expected = expectedResult;
+
+            //Assert
+            Assert.That(expected, Is.EqualTo(resultField.Text));            
+        }
+    }
+}
