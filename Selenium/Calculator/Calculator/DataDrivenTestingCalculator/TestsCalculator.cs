e51cd1f0cf91a80a78bdff77b067eb45d37f1c87
EvdokiyaValova <evdokiyavalova@gmail.com>
2022-06-11 12:02:23 +0300

    Test Calkulator

--- Selenium/Calculator/Calculator/DataDrivenTestingCalculator/TestsCalculator.cs
+++ Selenium/Calculator/Calculator/DataDrivenTestingCalculator/TestsCalculator.cs
@@ -7,15 +7,12 @@ namespace DataDrivenTestingCalculator
     public class TestsCalculator
     {
         private ChromeDriver driver;
-
-
-
        
         [OneTimeSetUp]
         public void Setup()
-        {
+        {            
             driver = new ChromeDriver();
-            driver.Url = "https://number-calculator.nakov.repl.co/";            
+            driver.Url = "https://number-calculator.nakov.repl.co/";
         }
 
         [OneTimeTearDown]
@@ -23,6 +20,7 @@ namespace DataDrivenTestingCalculator
         {
             driver.Quit();
         }
+
         [Test]
         public void Test_Calculator()
         {
@@ -70,7 +68,7 @@ namespace DataDrivenTestingCalculator
         [TestCase("0", "/", "4", "Result: 0")]
         [TestCase("4", "/", "0", "Result: Infinity")]
         [TestCase("3", "/", "1.5", "Result: 2")]
-        public void Test_CalculatorWebApp(string num1, string op, string num2, string expectedResult)
+        public void Test_CalculatorWebApp_ValidNumber(string num1, string op, string num2, string expectedResult)
         {
             //Arrange
             var field1 = driver.FindElement(By.Id("number1"));
@@ -90,6 +88,63 @@ namespace DataDrivenTestingCalculator
 
             //Assert
             Assert.That(expected, Is.EqualTo(resultField.Text));            
+        }
+
+        [TestCase("Text", "+", "1", "Result: invalid input")]
+        [TestCase("Text", "+", "Text", "Result: invalid input")]
+        [TestCase("2", "+", "Text", "Result: invalid input")]
+        [TestCase("2", "-", "", "Result: invalid input")]
+        [TestCase("", "-", "5", "Result: invalid input")]
+        [TestCase("1", "!", "5", "Result: invalid operation")]
+        [TestCase("1", "@", "5", "Result: invalid operation")]
+        [TestCase(",", "+", ",", "Result: invalid input")]
+        public void Test_CalculatorWebApp_InvalidNumber(string num1, string op, string num2, string expectedResult)
+        {
+            //Arrange
+            var field1 = driver.FindElement(By.Id("number1"));
+            var field2 = driver.FindElement(By.Id("number2"));
+            var operation = driver.FindElement(By.Id("operation"));
+            var calculateButton = driver.FindElement(By.Id("calcButton"));
+            var clearField = driver.FindElement(By.Id("resetButton"));
+            var resultField = driver.FindElement(By.Id("result"));
+
+            //Act
+            clearField.Click();
+            field1.SendKeys(num1);
+            operation.SendKeys(op);
+            field2.SendKeys(num2);
+            calculateButton.Click();
+            var expected = expectedResult;
+
+            //Assert
+            Assert.That(expected, Is.EqualTo(resultField.Text));
+        }
+
+        [Test]
+        public void Test_Calculator_Reset()
+        {
+            //Arrange
+            var field1 = driver.FindElement(By.Id("number1"));
+            var field2 = driver.FindElement(By.Id("number2"));
+            var operation = driver.FindElement(By.Id("operation"));
+            var calculateButton = driver.FindElement(By.Id("calcButton"));
+            var clearField = driver.FindElement(By.Id("resetButton"));
+            var resultField = driver.FindElement(By.Id("result"));
+
+            var page = field1.Text + field2.Text + operation.Text + resultField.Text;
+
+            //Act
+            field1.SendKeys("7");
+            operation.SendKeys("+");
+            field2.SendKeys("2");
+            calculateButton.Click();
+
+            //Assert
+            Assert.IsFalse(page == string.Empty);
+
+            clearField.Click();
+
+            Assert.IsTrue(page != string.Empty);
         }
     }
 }
